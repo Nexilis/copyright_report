42ca1341ebeb4914df7c28d3a9f2c1038fb66d48
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-02-09 15:46:30 +0100

    Extract user id

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod settings;
-use std::error::Error;
 use hyper::body;
-use hyper::{Client, Request, Method, Body};
+use hyper::{Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
-use serde_json::{Value};
+use serde_json::Value;
+use std::error::Error;
 use tokio;
 
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
@@ -43,8 +43,17 @@ async fn main() -> Result<()> {
     let body_bytes = body::to_bytes(res.into_body()).await?;
     let body = String::from_utf8(body_bytes.to_vec()).expect("response was not valid utf-8");
     let body_deserialized: Value = serde_json::from_str(&body)?;
+    let authenticated_user_id = body_deserialized
+        .as_object()
+        .unwrap()
+        .get("authenticatedUser")
+        .unwrap()
+        .get("id")
+        .unwrap()
+        .as_str()
+        .unwrap();
 
-    println!("{:#?}", body_deserialized);
+    println!("{:#?}", authenticated_user_id);
 
     Ok(())
     //https://docs.google.com/presentation/d/1QmWRwnKzclTZFn2h6tlMyjPaQVUCR9haoJd7NiIeONA/edit#slide=id.p
