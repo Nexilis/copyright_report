56cebd9d60d632435fd6a5f82c712ce1f32858af
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-03-15 17:09:12 +0100

    Moving of dates calculation to AzureApiConfig

--- src/azure_api_config.rs
+++ src/azure_api_config.rs
@@ -1,5 +1,11 @@
+use chrono::prelude::*;
+
 pub struct AzureApiConfig {
     pub uris: Uris,
+    /// e.g. 2020-12-31
+    pub date_from: String,
+    /// e.g. 2020-12-31
+    pub date_to: String,
 }
 
 pub struct Uris {
@@ -11,9 +17,11 @@ pub struct Uris {
 
 impl AzureApiConfig {
     #[inline(always)]
-    pub fn new(organization: &str) -> AzureApiConfig {
+    pub fn new(organization: &str, date_from_str: &str) -> AzureApiConfig {
         let base_url = "https://dev.azure.com/";
         let url_with_org = format!("{}/{}", base_url, organization);
+        let (date_from, date_to) = calculate_dates(&date_from_str);
+        println!("Dates: {} - {}", date_from, date_to);
 
         AzureApiConfig {
             uris: Uris {
@@ -22,6 +30,18 @@ impl AzureApiConfig {
                 pull_requests: format!("{}/_apis/git/pullRequests", &url_with_org),
                 commits: format!("{}/_apis/git/repositories/repo-id/commits", &url_with_org),
             },
+            date_from: date_from.to_string(),
+            date_to: date_to.to_string(),
         }
     }
+}
+
+fn calculate_dates(date_from_str: &str) -> (String, String) {
+    let date_from = DateTime::parse_from_rfc3339(date_from_str)
+        .expect("date_from needs to be formatted according to RFC3339");
+    let date_to = Local::now();
+    (
+        date_from.format("%Y-%m-%d").to_string(),
+        date_to.format("%Y-%m-%d").to_string(),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod azure_api_config;
 mod pull_requests;
 mod settings;
 
-use chrono::prelude::*;
 use std::error::Error;
 use tokio;
 
@@ -15,15 +14,14 @@ async fn main() -> Result<()> {
 
     let azure_settings = set.azure.unwrap();
     let org = azure_settings.organization.unwrap();
+    let date_from = set.report.unwrap().date_from.unwrap();
 
-    let azure_connection_data = azure_api_config::AzureApiConfig::new(&org);
-
-    let (_date_from, _date_to) = calculate_dates();
+    let azure_api_config = azure_api_config::AzureApiConfig::new(&org, &date_from);
 
     let pass: String = azure_settings.pass.unwrap();
     let auth_header: String = create_auth_header(&pass);
 
-    pull_requests::load_pull_requests(&azure_connection_data.uris.connection_data, &auth_header).await;
+    pull_requests::load_pull_requests(&azure_api_config.uris.connection_data, &auth_header).await;
 
     Ok(())
     //https://docs.google.com/presentation/d/1QmWRwnKzclTZFn2h6tlMyjPaQVUCR9haoJd7NiIeONA/edit#slide=id.p
@@ -33,13 +31,4 @@ fn create_auth_header(pass: &str) -> String {
     let secret = format!(":{}", pass);
     let secret_encoded = base64::encode(&secret);
     format!("Basic {}", secret_encoded)
-}
-
-fn calculate_dates() -> (String, String) {
-    let date_to = Local::now();
-    let date_from = date_to - chrono::Duration::days(30);
-    (
-        date_to.format("%Y-%m-%d").to_string(),
-        date_from.format("%Y-%m-%d").to_string(),
-    )
 }
