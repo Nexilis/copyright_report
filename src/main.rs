9e6b40c0b1e154d4c3bf1b89eca9f77a46f0d2ad
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-09-13 19:31:29 +0200

    Addition of tui create with a Block example

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,29 @@ mod settings;
 use std::error::Error;
 use tokio;
 
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear().err();
+    terminal.draw(|mut f| {
+        let size = f.size();
+        let mut block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render(&mut block, size);
+    }).err();
+
     let set = settings::read_from_file();
     println!("{:#?}", set);
 
